352740fb736b14608f7927ea103984719b92ca0c
hoangthienkhmt <hoangthienkhmt@gmail.com>
2024-11-07 22:24:17 +0700

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'https://a71e190a76786415d86832a46d6a8e5e-1959444601.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
